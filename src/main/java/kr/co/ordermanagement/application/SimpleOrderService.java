89af0ab3a6f7f9b8a76b6648e555c9a971c8c31f
xistne <du20063@gmail.com>
2025-02-13 17:05:36 +0900

    xistne : stream으로 수정

--- src/main/java/kr/co/ordermanagement/application/SimpleOrderService.java
+++ src/main/java/kr/co/ordermanagement/application/SimpleOrderService.java
@@ -27,28 +27,35 @@ public class SimpleOrderService {
         this.orderRepository = orderRepository;
     }
     public OrderResponseDto createOrder(List<OrderProductRequestDto> orderProductRequestDtos) {
-        // 1. 주문 가능한지 확인
-            // 1-1. 잘못된 상품번호로 요청하는경우
-            // 1-2. 주문된 상품의 재고수량이 부족한지 확인
-        // 2-1. 주문이 가능하다면 상품의 재고수량 줄이기
-        // 2-2. 주문이 불가능하다면 주문 실패
-        /* DESC: 원래 상품이 하나라도 없는경우, 수량이 부족한 상품이 있는 경우 2번의 조건을 가지려고 했음..
-            그러나 상품 findByAllId를 쓰려니 받은 상품정보를 idList로 만든 후 조회를 해야하고, 응답이 정상적으로 올 경우 그 데이터를 쓸 일이 없을 것 같아서 한번에 처리하기로 함...
-        * */
-        // DESC : 주문상품을 돌면서 해당상품이 아예없을 경우 주문실패(해당상품없음), 있을경우 배열에 해당 상품의 id를 저장해 놓았다가 주문실패(수량부족), 그 외 주문성공
-        List<OrderedProduct> orderedProducts = new ArrayList<>();
-        for(OrderProductRequestDto orderProductRequestDto : orderProductRequestDtos) {
-            Product product = this.productRepository.findById(orderProductRequestDto.getId());
-            // DESC : if문에서 orderProductRequestDto와 조회한 product를 비교하였는데 Product 개체의 함수를 이용하는걸로 변경
-            product.checkEnoughAmount(orderProductRequestDto.getAmount());
-            product.decreaseAmount(orderProductRequestDto.getAmount());
-            orderedProducts.add(new OrderedProduct(product.getId(),product.getName(), product.getPrice(), product.getAmount()));
-        }
-        // DESC : 주문 생성
+        List<OrderedProduct> orderedProducts = this.makeOrderedProducts(orderProductRequestDtos);
+        this.decreasesProductsAmount(orderedProducts);
         Order order = new Order(orderedProducts);
         Order savedOrder = this.orderRepository.add(order);
         OrderResponseDto orderResponseDto = OrderResponseDto.toDto(savedOrder);
         return orderResponseDto;
+    }
+
+    private void decreasesProductsAmount(List<OrderedProduct> orderedProducts) {
+        orderedProducts.forEach(orderedProduct -> {
+            Product product = this.productRepository.findById(orderedProduct.getId());
+            product.decreaseAmount(orderedProduct.getAmount());
+        });
+    }
+
+    private List<OrderedProduct> makeOrderedProducts(List<OrderProductRequestDto> orderProductRequestDtos) {
+        return orderProductRequestDtos.stream().map(orderProductRequestDto -> {
+            Long productId = orderProductRequestDto.getId();
+            Product product = this.productRepository.findById(productId);
+
+            product.checkEnoughAmount(orderProductRequestDto.getAmount());
+
+            return new OrderedProduct(
+                    product.getId(),
+                    product.getName(),
+                    product.getPrice(),
+                    orderProductRequestDto.getAmount()
+            );
+        }).toList();
     }
 
     public OrderResponseDto changeOrderState(Long orderId, ChangeStateRequestDto changeStateRequestDto) {
